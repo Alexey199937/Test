70620617baec36f2249d0c5f5434676e17c01549
Limboll <aleksej.myagkov.99@mail.ru>
2017-05-30 18:18:43 +0300

    Test.c

---
+++ Test.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define size 255
+#define CAN_NOT_OPEN_FILE 0
+#define NO_MEMORY 0
+#define SUCCESS 1
+#define INVALID_DATA 0
+#define NO_MP3 0
